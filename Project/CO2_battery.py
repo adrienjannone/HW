805e94bcb4602f1469217c1a512de60a76f6843c
gregoire.igor@gmail.com <Ms2vJpR98>
2025-12-01 17:14:26 +0100

    Discharge cycle

--- Project/CO2_battery.py
+++ Project/CO2_battery.py
@@ -14,11 +14,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 
-
+"""
+D1: Outlet of the liquid CO2 storage tank and inlet of the evaporator
+D2: Outlet of the evaporator and inlet of the HXD
+D3: Outlet of the HXD and inlet of the compressor
+D7: Outlet of the compressor and inlet of the turbine
+D8: Outlet of the turbine and inlet of the PCHX
+D9: Outlet of the PCHX and inlet of the CO2 gas storage dome
+C1: Storage dome 
+"""
 
 class CO2_battery(object):
     def __init__(self, inputs, params, plot):
-        pass
+        self.p_storage_co2_liquid = params['p_storage_co2_liquid']
+        self.T_storage_water = params['T_storage_water']
+        self.T_storage_TES = params['T_storage_TES']
+        self.T_amb = params['T_amb']
+        self.p_amb = params['p_amb']
+        self.p_dome = params['p_dome']
+        self.k_dome = params['k_dome']
+        self.eta_comp = params['eta_comp']
+        self.eta_turb = params['eta_turb']
+        self.eta_pump = params['eta_pump']
+        self.fluid = params['fluid']
+        self.Pe = inputs
+        self.plot = plot
 
+        # Outside states
+        self.p_0 = self.p_amb
+        self.T_0 = self.T_amb
+        self.h_0 = CP.PropsSI('H', 'P', self.p_0, 'T', self.T_0, self.fluid)
+        self.s_0 = CP.PropsSI('S', 'P', self.p_0, 'T', self.T_0, self.fluid)
+
+        # Dome states
+        self.p_C1 = self.p_dome
+        self.T_C1 = self.T_amb
+        self.h_C1 = CP.PropsSI('H', 'P', self.p_C1, 'T', self.T_C1, self.fluid)
+        self.s_C1 = CP.PropsSI('S', 'P', self.p_C1, 'T', self.T_C1, self.fluid)
+        self.e_C1 = self.h_C1 - self.h_0 - self.T_0 * (self.s_C1 - self.s_0)
+
+    def discharge_phase(self):
+        # Liquid CO2 storage
+        self.p_D1 = self.p_storage_co2_liquid
+        self.T_D1 = self.T_amb
+        self.h_D1 = CP.PropsSI('H', 'P', self.p_D1, 'T', self.T_D1, self.fluid)
+        self.s_D1 = CP.PropsSI('S', 'P', self.p_D1, 'T', self.T_D1, self.fluid)
+        self.e_D1 = self.h_D1 - self.h_0 - self.T_0 * (self.s_D1 - self.s_0)
+
+        # PCHX outlet - Dome inlet
+        self.p_D9 = self.p_dome / self.k_dome
+        
+
+
+        
+      
     def evaluate(self):
         pass
+
--- Project/CO2_battery_test.py
+++ Project/CO2_battery_test.py
@@ -25,17 +25,20 @@ eta_comp = 0.85                     # Compressor adiabatic efficiency (isentropi
 eta_turb = 0.9                      # Turbine adiabatic efficiency (isentropic)
 eta_pump = 0.85                     # Pump adiabatic efficiency (isentropic)
 pinch = 2 + 273.15                  # K (jsp où tu l'as trouvé)
+p_amb = 1e5                         # Pa
 inputs = Pe
 params = {
     'p_storage_co2_liquid': p_storage_co2_liquid,
     'T_storage_water': T_storage_water,
     'T_storage_TES': T_storage_TES,
     'T_amb': T_amb,
+    'p_amb': p_amb,
     'p_dome': p_dome,
     'k_dome': k_dome,
     'eta_comp': eta_comp,
     'eta_turb': eta_turb,
-    'eta_pump': eta_pump
+    'eta_pump': eta_pump,
+    'fluid': 'CO2'
 }
 
 
